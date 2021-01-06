e03fdae06ad5cbe0f62b817894985eb33fe05b3f
p1407846 <hbonnavaud@gmail.com>
2021-01-06 20:07:55 +0100

    nothing special

---
+++ src/ui/custom_widgets/canvas.py
@@ -0,0 +1,12 @@
+from PyQt5.QtWidgets import QWidget
+
+
+class Canvas(QWidget):
+    def __init__(self, parent=None):
+        super(Canvas, self).__init__(parent)
+
+    def drawRectangle(self, start_x, start_y, width, height):
+
+    def drawImage(self, start_x, start_y, width, height, img_path):
+
+
